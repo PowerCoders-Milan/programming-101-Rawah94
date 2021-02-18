f012a9a4e80fd89fbb0fddaa81569e16aea53b95
Rawah94 <78372274+Rawah94@users.noreply.github.com>
2021-02-18 12:16:48 +0100

    solved

--- 01-Blockly/blockly.js
+++ 01-Blockly/blockly.js
@@ -1,1 +1,17 @@
 //Insert level 9 code below
+
+Share your blocks with this link:
+
+https://blockly.games/maze?lang=en&level=9&skin=2#et34t6
+
+while (notDone()) {
+  if (isPathForward()) {
+    moveForward();
+  } else {
+    if (isPathLeft()) {
+      turnLeft();
+    }
+    moveForward();
+  }
+}
+
