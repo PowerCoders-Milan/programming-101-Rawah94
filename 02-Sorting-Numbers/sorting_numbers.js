fed22ee448cf2e496d6e239564ff4cc9f2def31c
Rawah94 <78372274+Rawah94@users.noreply.github.com>
2021-02-18 15:26:33 +0100

    Update sorting_numbers.js

--- 02-Sorting-Numbers/sorting_numbers.js
+++ 02-Sorting-Numbers/sorting_numbers.js
@@ -7,3 +7,18 @@ print digits
 */
 
 //insert your code below
+function listsGetSortCompare(type, direction) {
+  var compareFuncs = {
+    "NUMERIC": function(a, b) {
+        return Number(a) - Number(b); },
+    "TEXT": function(a, b) {
+        return a.toString() > b.toString() ? 1 : -1; },
+    "IGNORE_CASE": function(a, b) {
+        return a.toString().toLowerCase() > b.toString().toLowerCase() ? 1 : -1; },
+  };
+  var compare = compareFuncs[type];
+  return function(a, b) { return compare(a, b) * direction; }
+}
+
+
+window.alert([1, 8, 6].slice().sort(listsGetSortCompare("NUMERIC", 1)));
